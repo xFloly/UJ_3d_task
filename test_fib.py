2662e73efd29a5e8468af1da60fd283718c2150f
Filip Soszyński <101994826+xFloly@users.noreply.github.com>
2025-04-03 08:52:01 +0200

    jak liczyc tutorial

--- test_fib.py
+++ test_fib.py
@@ -4,7 +4,7 @@ from fib import fib
 class TestFibonacci(unittest.TestCase):
 
     def test_fibonacci_correct(self):
-        self.assertEqual(fib(5), 3)
+        self.assertEqual(fib(5), 5)
 
     # def test_fibonacci_incorrect(self):
     #     self.assertEqual(fib(5), 10)
